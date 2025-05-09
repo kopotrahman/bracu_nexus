06cb4cec3f01fa4a41e62b2b0836a6017f823018
Muztahid Durjoy <muztahiddurjoy99@gmail.com>
2025-05-09 17:00:28 +0600

    dfdff

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,54 @@
 <?php
-  echo 
+  echo '    <!-- Footer -->
+    <footer class="bg-slate-900 pb-3 pt-10 lg:pt-20 text-white display-flex justify-center">
+
+      <div class="flex flex-wrap justify-between gap-y-16 gap-x-7 mb-16">
+
+        <!-- About -->
+        <div class="text-center mx-2 mb-5">
+          <h2 class="font-bold text-2xl mb-3 tracking-wider">Events</h2>
+          <p class="pt-1">There\'s no upcoming events</p>
+
+          <!-- Get Icons From Wherever and use it here -->
+          <div class="flex flex-wrap gap-x-7 justify-center gap-y-5 mt-5">
+            <a href="#!" class="text-primary-color hover:opacity-90">
+              icon
+            </a>
+            <a href="#!" class="text-primary-color hover:opacity-90">
+              icon
+            </a>
+            <a href="#!" class="text-primary-color hover:opacity-90">
+              icon
+            </a>
+          </div>
+        </div>
+
+        <div class="flex flex-wrap gap-y-8">
+        <!-- Heading -->
+        <div class="mx-10 lg:mx-16">
+          <h2 class="font-bold text-xl mb-4">Menu</h2>
+          <ul class="flex flex-col justify-space-between gap-y-2 text-left font-semibold text-gray-400">
+            <li><a href="public\pages\section-swap.html" class="hover:text-primary-color">Section Swap</a></li>
+            <li><a href="#!" class="hover:text-primary-color">Club Information</a></li>
+            <li><a href="#!" class="hover:text-primary-color">Research Details</a></li>
+            <li><a href="#!" class="hover:text-primary-color">Events</a></li>
+          </ul>
+        </div>
+
+      
+
+
+      
+
+      </div>
+
+      
+      <!-- Sub Footer -->
+      <div class="h-1 w-full bg-gradient-to-r from-primary-color to-green-500 "></div>
+      <!-- Copyright -->
+      <p class="text-center mt-8 mb-4 font-semibold">Template Raven - Copyright &copy; <span id="year"></span>. All
+        Rights
+        Reserved</p>
+
+    </footer>'
 ?>
